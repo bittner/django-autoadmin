f503c241991b7ef3c403b1b36749ccda819dff2b
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-10-16 02:52:06 -0400

    PEP8 cleanups.

--- autoadmin/migrations/0001_initial.py
+++ autoadmin/migrations/0001_initial.py
@@ -15,10 +15,30 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='AutoAdminSingleton',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('password', models.CharField(max_length=128, null=True, verbose_name='Password', blank=True)),
-                ('password_hash', models.CharField(max_length=128, null=True, verbose_name='Password hash', blank=True)),
-                ('account', models.ForeignKey(verbose_name='Account', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+                (
+                    'id', models.AutoField(
+                        verbose_name='ID', serialize=False, auto_created=True,
+                        primary_key=True
+                    )
+                ),
+                (
+                    'password', models.CharField(
+                        max_length=128, null=True, verbose_name='Password',
+                        blank=True
+                    )
+                ),
+                (
+                    'password_hash', models.CharField(
+                        max_length=128, null=True,
+                        verbose_name='Password hash', blank=True
+                    )
+                ),
+                (
+                    'account', models.ForeignKey(
+                        verbose_name='Account', blank=True,
+                        to=settings.AUTH_USER_MODEL, null=True
+                    )
+                ),
             ],
             options={
                 'verbose_name': 'Autoadmin properties',
